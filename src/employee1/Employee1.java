630c374f41f5593d2d063762e3912ed2f2687a04
Adarsh M P <adarshm09@gmail.com>
2015-10-27 11:55:49 +0530

    First commit

---
+++ src/employee1/Employee1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employee1;
+
+/**
+ *
+ * @author ADARSH
+ */
+public class Employee1 {
+   private static double salary;
+   public static final String DEPARTMENT = "Devolopment";
+   
+              
+  
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   salary = 1000;
+   System.out.println(DEPARTMENT + "\s average salary is :" +salary);
+   
+               
+        
+              // TODO code application logic here
+    }
+
+    //private void setSalary(int i) {
+      //  throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+   // }
+    
+}
